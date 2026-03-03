623939f10bc19609296a1b54a5e3fc9ee320589f
Abhiram1108 <abhiramboddupalli11@gmail.com>
2026-03-03 09:40:48 +0530

    Added Level 3 Java Practice Programs

---
+++ level3/FahrenheitToCelsius.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class FahrenheitToCelsius {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        double fahrenheit;
+
+        System.out.print("Enter temperature in Fahrenheit: ");
+        fahrenheit = input.nextDouble();
+
+        double celsiusResult = (fahrenheit - 32) * 5 / 9;
+
+        System.out.println("The " + fahrenheit +
+                " fahrenheit is " + celsiusResult +
+                " celsius");
+
+        input.close();
+    }
+}
